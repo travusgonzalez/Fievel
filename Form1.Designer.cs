b19eb515ed20cbaf68244633638514317167fdf5
Travus Gonzalez <travusgonzalez@outlook.com>
2021-08-27 13:23:04 -0400

    Initial upload
    v0.0.1 upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Fievel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_status = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status: ";
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.ForeColor = System.Drawing.Color.DarkRed;
+            this.label_status.Location = new System.Drawing.Point(72, 35);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(75, 15);
+            this.label_status.TabIndex = 2;
+            this.label_status.Text = "Not Running";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 110);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Fievel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_status;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fievel
+{
+    public partial class Form1 : Form
+    {
+        private bool IsRunning { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            IsRunning = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IsRunning = !IsRunning;
+            if (IsRunning && backgroundWorker1.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+                button1.Text = "Stop";
+                label_status.Text = "Running";
+                label_status.ForeColor = Color.DarkGreen;
+                
+            }
+            else
+            {
+                backgroundWorker1.CancelAsync();
+                button1.Text = "Start";
+                label_status.Text = "Not Running";
+                label_status.ForeColor = Color.DarkRed;
+            }
+        }
+
+        private void MoveMouse(int distance = 20, bool direction = false)
+        {
+            var f = Form.ActiveForm;
+            
+            Action a = () =>
+            {
+                Cursor = new Cursor(Cursor.Current.Handle);
+
+                if (direction)
+                {
+                    Cursor.Position = new Point(Cursor.Position.X + distance, Cursor.Position.Y);
+                }
+                else
+                {
+                    Cursor.Position = new Point(Cursor.Position.X - distance, Cursor.Position.Y);
+                }
+            };
+
+            if (f.InvokeRequired)
+            {
+                f.Invoke(a);
+            }
+            else
+            {
+                a();
+            }
+
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            bool toggle = false;
+
+            while(true)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    MoveMouse(1, toggle);
+                    toggle = !toggle;
+
+                    System.Threading.Thread.Sleep(500);
+                }
+            }
+        }
+
+        private void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+    }
+}
